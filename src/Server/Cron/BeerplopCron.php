2d51d70cebf9553cd215a42a47ba0b1aa6604843
Enno Woortmann <enno.woortmann@web.de>
2019-12-08 14:59:01 +0100

    Fix Cron, update to Twig 3

--- src/Server/Cron/BeerplopCron.php
+++ src/Server/Cron/BeerplopCron.php
@@ -5,8 +5,10 @@ use Jobby\Jobby;
 use WOLSoftCore\Server\App;
 use WOLSoftCore\Server\Utils\ActivityStreamLogger;
 
+$confPath = __DIR__ . '/../../../config/Conf.php';
+
 require_once __DIR__ . '/../../../vendor/autoload.php';
-require_once __DIR__ . '/../../../config/Conf.php';
+require_once $confPath;
 
 $jobby = new Jobby();
 
@@ -14,7 +16,10 @@ $jobby = new Jobby();
 App::getInstance('beerplop');
 
 $jobby->add('NotifyOfflineSaveStates', [
-    'closure' => function () {
+    'closure' => function () use ($confPath) {
+        // as the cron is executed in a separate process teh config needs to be included again
+        require_once $confPath;
+
         $notifiedSaveStates = (new SaveStateService())->notifyOfflineSaveStates();
 
         if ($notifiedSaveStates > 0) {
