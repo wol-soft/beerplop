6309db5be8f52d012275ef1900661b3ea35746c8
Enno Woortmann <enno.woortmann@web.de>
2019-12-19 19:03:40 +0100

    Added additional Beer Factory tests including upgrades, transport capacity, stone, handling of multiple materials for a single queue entry, extended manual production

--- tests/client/Minigames/BeerFactory/BeerFactoryTest.js
+++ tests/client/Minigames/BeerFactory/BeerFactoryTest.js
@@ -261,6 +261,11 @@ describe('Beer Factories', function () {
             expect(parseInt($('#beer-factory__stock__amount-wood').text()) + parseInt($('#beer-factory__stock__amount-strongWood').text())).to.equal(5);
         });
 
+        it("Should increase the amount of manually produced items", function () {
+            $('#beer-factory__wood').find('.beer-factory__manual-harvest').trigger('click');
+            expect(beerFactory.state.getMaterial('wood').amount + beerFactory.state.getMaterial('strongWood').amount).to.equal(7);
+        });
+
         it("Should trigger the EVENTS.BEER_FACTORY.QUEUE.MATERIAL_FINISHED event after a material is finished", async function () {
             await awaitEventEmitted(gameEventBusBuildQueueMaterialFinishedSpy, 1);
 
@@ -628,6 +633,289 @@ describe('Beer Factories', function () {
                             .match(/\d+ \/ (\d+)/)[1]
                     ).to.equal('150')
             );
+        });
+    });
+
+    describe("The transport extension and Quarry", function () {
+        let gameEventBusBuildQueueMaterialFinishedSpy = sinon.spy();
+
+        // current state here: 3 Lumberjacks, 1 Storage Place
+        it("Should unlock at four Lumberjacks and three Storage Places", function () {
+            // button reference must be fetched again as the UI is updated after adding the first job
+            $('#beer-factory__wood').find('.beer-factory__queue-build').trigger('click');
+            $('#beer-factory__storage').find('.beer-factory__queue-build').trigger('click');
+            $('#beer-factory__storage').find('.beer-factory__queue-build').trigger('click');
+
+            const buildQueue = beerFactory.state.getBuildQueue();
+
+            buildQueue[0].deliveredItems = 174;
+            buildQueue[0].materials[0].delivered = 174;
+
+            buildQueue[1].deliveredItems = 188;
+            buildQueue[1].materials[0].delivered = 134;
+            buildQueue[1].materials[1].delivered = 54;
+
+            buildQueue[2].deliveredItems = 341;
+            buildQueue[2].materials[0].delivered = 243;
+            buildQueue[2].materials[1].delivered = 98;
+
+            beerFactory.state.getMaterial('wood').amount = 3;
+
+            gameEventBus.emit(EVENTS.CORE.ITERATION);
+
+            expect(beerFactory.state.getBuildQueue().length).to.equal(0);
+            expect(beerFactory.state.getFactory('wood').amount).to.equal(4);
+            expect(beerFactory.state.getFactory('storage').amount).to.equal(3);
+
+            expect($('.beer-factory__building-container').length, 'Wrong amount of factories unlocked').to.equal(4);
+            expect($('#beer-factory__transport').length, 'Transport not unlocked').to.equal(1);
+            expect($('#beer-factory__stone').length, 'Quarry not unlocked').to.equal(1);
+        });
+
+        it("Should unlock stone as a new material", function () {
+            expect($('#beer-factory__stock').find('.beer-factory__stock__container').length).to.equal(3);
+            expect($('#beer-factory__stock__amount-stone').text()).to.equal('0');
+        });
+
+        it("Should be possible to add a quarry to the Build Queue", function () {
+            $('#beer-factory__stone').find('.beer-factory__queue-build').trigger('click');
+
+            const buildQueue = beerFactory.state.getBuildQueue();
+
+            expect(buildQueue.length, 'Build Queue empty').to.equal(1);
+
+            expect(buildQueue[0].item).to.equal('stone');
+            expect(buildQueue[0].action).to.equal(BUILD_QUEUE__BUILD);
+            expect(buildQueue[0].requiredItems).to.equal(600);
+
+            expect(buildQueue[0].materials.length).to.equal(2);
+            expect(buildQueue[0].materials[0].key).to.equal('strongWood');
+            expect(buildQueue[0].materials[0].required).to.equal(100);
+            expect(buildQueue[0].materials[1].key).to.equal('wood');
+            expect(buildQueue[0].materials[1].required).to.equal(500);
+        });
+
+        it("Should trigger the EVENTS.BEER_FACTORY.QUEUE.MATERIAL_FINISHED event after a material is finished but the job isn't finished", async function () {
+            gameEventBus.on(EVENTS.BEER_FACTORY.QUEUE.MATERIAL_FINISHED, gameEventBusBuildQueueMaterialFinishedSpy);
+
+            beerFactory.state.getMaterial('wood').amount = 3;
+            beerFactory.state.getMaterial('strongWood').amount = 3;
+
+            const buildQueue = beerFactory.state.getBuildQueue();
+
+            buildQueue[0].deliveredItems = 98;
+            buildQueue[0].materials[0].delivered = 98;
+
+            gameEventBus.emit(EVENTS.CORE.ITERATION);
+
+            await awaitEventEmitted(gameEventBusBuildQueueMaterialFinishedSpy, 1);
+
+            expect(buildQueue[0].deliveredItems).to.equal(101);
+            expect(buildQueue[0].materials[0].delivered).to.equal(100);
+            expect(buildQueue[0].materials[1].delivered).to.equal(1);
+
+            expect(gameEventBusBuildQueueMaterialFinishedSpy.callCount).to.equal(1);
+            expect(gameEventBusBuildQueueMaterialFinishedSpy.getCall(0).args[1]).to.equal(0);
+            expect(gameEventBusBuildQueueMaterialFinishedSpy.getCall(0).args[2]).to.equal('strongWood');
+
+            gameEventBus.off(EVENTS.BEER_FACTORY.QUEUE.MATERIAL_FINISHED, gameEventBusBuildQueueMaterialFinishedSpy);
+        }).timeout(1000);
+
+        it("Should mark finished materials in the Build Queue", function () {
+            const strongWoodContainer = $('#build-queue__item-0__material-strongWood__container'),
+                  woodContainer = $('#build-queue__item-0__material-wood__container');
+
+            expect(strongWoodContainer.length, "Strong wood not displayed").to.equal(1);
+            expect(woodContainer.length, "Wood not displayed").to.equal(1);
+
+            expect(strongWoodContainer.hasClass('item__material-delivery__complete')).to.equal(true);
+            expect(woodContainer.hasClass('item__material-delivery__complete')).to.equal(false);
+            expect(strongWoodContainer.hasClass('item__material-delivery__in-progress')).to.equal(false);
+            expect(woodContainer.hasClass('item__material-delivery__in-progress')).to.equal(true);
+        });
+
+        it("Should not display finished materials if they should be hidden", function () {
+            $('#beer-factory__build-queue__hide-completed-materials').trigger('click');
+
+            const strongWoodContainer = $('#build-queue__item-0__material-strongWood__container'),
+                  woodContainer = $('#build-queue__item-0__material-wood__container');
+
+            expect(strongWoodContainer.length, "Strong wood displayed").to.equal(0);
+            expect(woodContainer.length, "Wood not displayed").to.equal(1);
+        });
+
+        it("Should display finished materials again after the hide switch is toggled again", function () {
+            $('#beer-factory__build-queue__hide-completed-materials').trigger('click');
+
+            const strongWoodContainer = $('#build-queue__item-0__material-strongWood__container'),
+                  woodContainer = $('#build-queue__item-0__material-wood__container');
+
+            expect(strongWoodContainer.length, "Strong wood displayed").to.equal(1);
+            expect(woodContainer.length, "Wood not displayed").to.equal(1);
+        });
+
+        it("Should produce stone after being finished", function () {
+            beerFactory.state.getMaterial('wood').amount = 3;
+
+            const buildQueue = beerFactory.state.getBuildQueue();
+
+            buildQueue[0].deliveredItems = 598;
+            buildQueue[0].materials[1].delivered = 498;
+
+            gameEventBus.emit(EVENTS.CORE.ITERATION);
+
+            expect(beerFactory.state.getBuildQueue().length).to.equal(0);
+            expect(beerFactory.state.getFactory('stone').amount, 'Factory not constructed').to.equal(1);
+
+            expect(beerFactory.state.getMaterial('stone').amount, 'Stone not produced').to.equal(1);
+            expect(beerFactory.state.getMaterial('stone').total, 'Stone not added to total produced amount').to.equal(1);
+        });
+
+        it("Should be possible to add a Transport System to the Build Queue", function () {
+            $('#beer-factory__transport').find('.beer-factory__queue-build').trigger('click');
+
+            const buildQueue = beerFactory.state.getBuildQueue();
+
+            expect(buildQueue.length, 'Build Queue empty').to.equal(1);
+
+            expect(buildQueue[0].item).to.equal('transport');
+            expect(buildQueue[0].action).to.equal(BUILD_QUEUE__BUILD);
+            expect(buildQueue[0].requiredItems).to.equal(330);
+
+            expect(buildQueue[0].materials.length).to.equal(3);
+            expect(buildQueue[0].materials[0].key).to.equal('strongWood');
+            expect(buildQueue[0].materials[0].required).to.equal(30);
+            expect(buildQueue[0].materials[1].key).to.equal('stone');
+            expect(buildQueue[0].materials[1].required).to.equal(50);
+            expect(buildQueue[0].materials[2].key).to.equal('wood');
+            expect(buildQueue[0].materials[2].required).to.equal(250);
+        });
+
+        it("Should raise the transport capacity after a Transport System is finished", function () {
+            beerFactory.state.getMaterial('wood').amount = 3;
+
+            const buildQueue = beerFactory.state.getBuildQueue();
+
+            buildQueue[0].deliveredItems = 328;
+            buildQueue[0].materials[0].delivered = 30;
+            buildQueue[0].materials[1].delivered = 50;
+            buildQueue[0].materials[2].delivered = 248;
+
+            gameEventBus.emit(EVENTS.CORE.ITERATION);
+
+            expect(beerFactory.state.getBuildQueue().length).to.equal(0);
+            expect(beerFactory.state.getFactory('transport').amount, 'Factory not constructed').to.equal(1);
+
+            expect($('#beer-factory__delivery-capacity').text(), 'Displayed transport capacity not updated').to.equal('5');
+        });
+
+        it("Should transport with the extended transport capacity", function () {
+            beerFactory.state.getMaterial('wood').amount = 10;
+            beerFactory.state.getMaterial('strongWood').amount = 0;
+
+            $('#beer-factory__wood').find('.beer-factory__queue-build').trigger('click');
+
+            gameEventBus.emit(EVENTS.CORE.ITERATION);
+
+            const buildQueue = beerFactory.state.getBuildQueue();
+
+            expect(buildQueue[0].deliveredItems).to.equal(5);
+            expect(buildQueue[0].materials[0].delivered).to.equal(5);
+
+            // 10 minus 5 plus 4 produced
+            expect(beerFactory.state.getMaterial('wood').amount + beerFactory.state.getMaterial('strongWood').amount).to.equal(9);
+        });
+    });
+
+    describe("A Lumberjack upgrade", function () {
+        it("Should not be added to the Build Queue when being locked", function () {
+            $($('#beer-factory__wood').find('.card')[0]).trigger('click');
+
+            expect(beerFactory.state.getBuildQueue().length).to.equal(1);
+        });
+
+        // additional precondition: 1 quarry, already built.
+        it("Should unlock after finishing the fifth Lumberjack", function () {
+            const buildQueue = beerFactory.state.getBuildQueue();
+
+            buildQueue[0].deliveredItems = 314;
+            buildQueue[0].materials[0].delivered = 314;
+
+            beerFactory.state.getMaterial('wood').amount = 3;
+
+            gameEventBus.emit(EVENTS.CORE.ITERATION);
+
+            expect(beerFactory.state.getBuildQueue().length).to.equal(0);
+            expect(beerFactory.state.getFactory('wood').amount).to.equal(5);
+
+            // the first upgrade path should be unlocked
+            const upgradeCards = $('#beer-factory__wood').find('.beer-factory__upgrade');
+            expect($(upgradeCards[0]).find('.beer-factory__upgrade__locked').hasClass('d-none'), 'first upgrade path locked state visible').to.equal(true);
+            expect($(upgradeCards[0]).find('.beer-factory__upgrade__locked + div').hasClass('d-none'), 'first upgrade path not visible').to.equal(false);
+
+            expect($(upgradeCards[1]).find('.beer-factory__upgrade__locked').hasClass('d-none'), 'second upgrade path locked state not visible').to.equal(false);
+            expect($(upgradeCards[1]).find('.beer-factory__upgrade__locked + div').hasClass('d-none'), 'second upgrade path visible').to.equal(true);
+        });
+
+        it("Should be added to the Build Queue if clicked after unlocking", function () {
+            $($('#beer-factory__wood').find('.card')[0]).trigger('click');
+
+            const buildQueue = beerFactory.state.getBuildQueue();
+
+            expect(buildQueue.length).to.equal(1);
+
+            expect(buildQueue[0].item).to.deep.equal({
+                factory: 'wood',
+                upgrade: 'double',
+                level: 1,
+            });
+
+            expect(buildQueue[0].action).to.equal(BUILD_QUEUE__UPGRADE);
+            expect(buildQueue[0].requiredItems).to.equal(4125);
+
+            expect(buildQueue[0].materials.length).to.equal(3);
+            expect(buildQueue[0].materials[0].key).to.equal('strongWood');
+            expect(buildQueue[0].materials[0].required).to.equal(375);
+            expect(buildQueue[0].materials[1].key).to.equal('stone');
+            expect(buildQueue[0].materials[1].required).to.equal(1750);
+            expect(buildQueue[0].materials[2].key).to.equal('wood');
+            expect(buildQueue[0].materials[2].required).to.equal(2000);
+        });
+
+        it("Should lock the next stage", function () {
+            const upgradeCards = $('#beer-factory__wood').find('.beer-factory__upgrade');
+
+            expect($(upgradeCards[0]).find('.beer-factory__upgrade__locked').hasClass('d-none'), 'first upgrade path locked not state visible').to.equal(false);
+            expect($(upgradeCards[0]).find('.beer-factory__upgrade__locked + div').hasClass('d-none'), 'first upgrade path visible').to.equal(true);
+
+            expect($(upgradeCards[1]).find('.beer-factory__upgrade__locked').hasClass('d-none'), 'second upgrade path locked state not visible').to.equal(false);
+            expect($(upgradeCards[1]).find('.beer-factory__upgrade__locked + div').hasClass('d-none'), 'second upgrade path visible').to.equal(true);
+        });
+
+        it("Should double the wood production after the job has been finished", function () {
+            beerFactory.state.getMaterial('stone').amount = 10;
+            beerFactory.state.getMaterial('wood').amount = 5;
+            beerFactory.state.getMaterial('strongWood').amount = 5;
+
+            const buildQueue = beerFactory.state.getBuildQueue();
+
+            buildQueue[0].deliveredItems = 4120;
+            buildQueue[0].materials[0].delivered = 375;
+            buildQueue[0].materials[1].delivered = 1745;
+            buildQueue[0].materials[2].delivered = 2000;
+
+            gameEventBus.emit(EVENTS.CORE.ITERATION);
+
+            expect(beerFactory.state.getBuildQueue().length).to.equal(0);
+            // 10 in stock, 5 consumed, 1 produced
+            expect(beerFactory.state.getMaterial('stone').amount).to.equal(6);
+            // 10 in stock, 0 consumed, 10 produced
+            expect(beerFactory.state.getMaterial('wood').amount + beerFactory.state.getMaterial('strongWood').amount).to.equal(20);
+        });
+
+        it("Should increase the amount of manually produced items", function () {
+            $('#beer-factory__wood').find('.beer-factory__manual-harvest').trigger('click');
+            expect(beerFactory.state.getMaterial('wood').amount + beerFactory.state.getMaterial('strongWood').amount).to.equal(23);
         });
     });
 });
