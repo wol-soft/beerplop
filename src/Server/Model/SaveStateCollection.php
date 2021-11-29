db76c56494295a88a22d68846d024f9dcde14428
Enno Woortmann <enno.woortmann@web.de>
2021-11-29 17:02:43 +0100

    PHP8.1 compatibility

--- src/Server/Model/SaveStateCollection.php
+++ src/Server/Model/SaveStateCollection.php
@@ -11,11 +11,10 @@ use WOLSoftCore\Server\Model\Collection\WOLSoftModelCollection;
  */
 class SaveStateCollection extends WOLSoftModelCollection
 {
-    public    const COLLECTION_MODEL = SaveState::class;
+    public const COLLECTION_MODEL = SaveState::class;
 
     protected const TABLE    = 'beerplop_savestate';
     protected const DATABASE = 'beerplop';
 
-    /** @var string */
-    protected $_primaryColumn = 'userId';
+    protected string $primaryColumn = 'userId';
 }
